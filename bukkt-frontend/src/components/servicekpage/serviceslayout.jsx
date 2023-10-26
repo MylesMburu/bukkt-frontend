3a6ff2b6eec582f7abaa8ec8d2de4bd51c0b3b93
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-10-26 16:30:16 +0300

    style  the service page

--- bukkt-frontend/src/components/servicekpage/serviceslayout.jsx
+++ bukkt-frontend/src/components/servicekpage/serviceslayout.jsx
@@ -1,59 +1,62 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 
 const Services = () => {
   const navigate = useNavigate();
-  const [buttonClicked, setButtonClicked] = useState(false);
 
   const handleClick = () => {
-    // Change the color from blue to white when clicked
-    setButtonClicked(true);
-
-    // Navigate to the corresponding service route
-    navigate(`/${Queue}`);
+    navigate(`/queue`);
   };
 
   return (
     <div>
    
       <div>
-        <h2>WELCOME TO JAMHURI MOTORS</h2>
-        <h3>Choose your service </h3>
+        <h2 className='text-center'>WELCOME TO JAMHURI MOTORS</h2>
+        <h3 className='text-center'>Choose your service </h3>
       </div>
       <div>
-        <button
-          className={buttonClicked ? 'white-button' : ''}
-          onClick={() => handleClick()}
-        >
-            <Link to="/Queue" >Service</Link> 
-        </button>
-        <button
-          className={buttonClicked ? 'white-button' : ''}
-          onClick={() => handleClick()}
-        >
-           <Link to="/Queue" >Break System Inspection</Link> 
-        </button>
-        <button
-          className={buttonClicked ? 'white-button' : ''}
-          onClick={() => handleClick()}
-        >
-       <Link to="/Queue" > Tire Services </Link> 
-        </button>
-        <button
-          className={buttonClicked ? 'white-button' : ''}
-          onClick={() => handleClick()}
-        >
-          <Link to="/Queue" > Engine Diagnostics</Link>   
-        </button>
-        <button
-          className={buttonClicked ? 'white-button' : ''}
-          onClick={() => handleClick()}
-        >
-         <Link to="/Queue" > Battery Services</Link>   
-        </button>
+        <div className='flex flex-col justify-center'>
+          {services.map((service) => (
+            <div
+              key={service.id}
+              className='flex flex-col justify-center items-center m-4'
+            >
+              <button
+                onClick={handleClick}
+                className={`bg-blue-500 text-white font-bold py-2 px-4 rounded`}
+              >
+                {service.name}
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 export default Services;
+
+
+const services = [{
+  id: 1, 
+  name: 'Servicing',
+
+},
+{
+  id: 2, 
+  name: 'Brake System Inspection',
+},
+{
+  id: 3, 
+  name: 'Tire Services',
+},
+{
+  id: 4, 
+  name: 'Engine Diagnostics',
+},
+{
+  id: 5, 
+  name: 'Battery Services',
+}
+]
